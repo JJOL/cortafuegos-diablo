e8bf16ea1ea59b11a3cd92e7969894f9de02c7d6
jjol <jjoulk@gmail.com>
2021-05-26 09:55:24 -0500

    Fix setup pox

--- setup_pox.sh
+++ setup_pox.sh
@@ -2,10 +2,8 @@
 
 POX_DIR=/home/mininet/pox
 CF_DIR=/home/mininet/cortafuegos-diablo
-
 cp $CF_DIR/controller/ids_controller_p2.py $POX_DIR/ext/ids_controller.py
-
-pip2 install -r controller/requirements.txt
+pip2 install -r requests
 
 cd $POX_DIR
-python2 pox.py log.level --DEBUG forwarding.hub --reactive=False ids_controller --apiip=<192.168.1.68>
+python2 pox.py log.level --INFO forwarding.hub --reactive=False ids_controller --apiip=<192.168.1.68>
